a590d9ca5705bc97f3be69261323810a85a65bd2
Samuel Cornet <samuel@noukod.com>
2018-09-20 13:24:20 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,3 +10,15 @@ def reverse_each_word(str)
   return rev_word_arr.join(' ')
 end
 reverse_each_word(str)
+
+
+def reverse_each_word(str)
+  rev_word_array = []
+  first_array_ = str.split(' ')
+  first_array_.collect do |word|
+  rev_word = word.reverse
+  rev_word_array << rev_word
+  end
+  return rev_word_array.join(' ')
+end
+reverse_each_word(str)
